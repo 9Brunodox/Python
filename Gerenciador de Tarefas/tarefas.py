93a5d8f48b9954eb876ccfc81a217636c218262c
Bruno <103011932+9Brunodox@users.noreply.github.com>
2024-11-25 12:42:28 -0300

    Add files via upload

--- Gerenciador de Tarefas/tarefas.py
+++ Gerenciador de Tarefas/tarefas.py
@@ -9,11 +9,25 @@ def ver_tarefas(tarefas):
     for indice, tarefa in enumerate(tarefas):
         status = "✓" if tarefa["Completa"] else "x"
         nome_tarefa = tarefa["Tarefa"]
-        print(f"{indice}. [{status}] {nome_tarefa}")
+        indice_ajustado = indice + 1
+        print(f"{indice_ajustado}. [{status}] {nome_tarefa}")
 
 def atualizar_nome_tarefas(tarefas, indice, novo_nome):
     tarefas[indice] = {"Tarefa": novo_nome, "Completa": False}
     print("Novo da tarefa atualizado!")
+    
+def completar_tarefas(tarefas, indice):
+    indice_ajustado = int(indice) - 1
+    tarefas[indice_ajustado]["Completa"] = True
+    print(f"Tarefa {indice} marcada como completa!")
+    return
+
+def deletar_tarefas_completadas(tarefas):
+    for tarefa in tarefas:
+        if tarefa["Completa"]:
+            tarefas.remove(tarefa)
+        print("As tarefas que foram completadas foram excluídas com sucesso!")
+    return
         
 tarefas = []
 while True:
@@ -36,6 +50,13 @@ while True:
         numero_tarefa = int(input("Escolha a tarefa: "))
         novo_nome = input("Digite o novo nome da tarefa: ")
         atualizar_nome_tarefas(tarefas, numero_tarefa, novo_nome)
+    elif escolha == 4:
+        ver_tarefas(tarefas)
+        indice = int(input("Digite o número da tarefa que deseja marcar como completa: "))
+        completar_tarefas(tarefas, indice)
+    elif escolha == 5:
+        deletar_tarefas_completadas(tarefas)
+        ver_tarefas(tarefas)
     elif escolha == 6:
         break
 
